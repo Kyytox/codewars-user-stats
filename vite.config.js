a08a49097d0897fd80d26ef23bb204f1a509f11a
Kyytox <thomas.caillault@protonmail.com>
2022-11-22 21:02:42 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "https://kyytox.github.io/",
+    base: "/codewars-user-stats",
     // base: "http://127.0.0.1:5000/",
 });
